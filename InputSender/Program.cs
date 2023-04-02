ef98e0e7286c72f1150da25a7607ee6a35725cee
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-04-02 15:57:47 +0200

    add sender

---
+++ InputSender/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, World!");
+
+Console.Title = "Test2 sender";
+Console.WriteLine("Test2 sender. Queue: MyQueue.1");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 8180,
+    UserName = "guest",
+    Password = "guest"
+}
