0c09ac24d54b4c8519c359e65e7d7df42b71405a
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-04-05 07:29:17 +0200

    update second sencer

--- InputSender/Program.cs
+++ InputSender/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RabbitMQ.Client;
+using System.Text;
 
 Console.WriteLine("Hello, World!");
 
 Console.Title = "Test2 sender";
-Console.WriteLine("Test2 sender. Queue: MyQueue.1");
+Console.WriteLine("Test2 sender. Queue: test.2.queue");
 
 var factory = new ConnectionFactory()
 {
@@ -12,4 +13,36 @@ var factory = new ConnectionFactory()
     Port = 8180,
     UserName = "guest",
     Password = "guest"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(
+        queue: "test.2.queue",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    while (true)
+    {
+        Console.WriteLine("write sms content");
+        Console.WriteLine("type exit to exit");
+        string content = Console.ReadLine();
+        if (content.Equals("exit"))
+        {
+            break;
+        }
+        var body = Encoding.UTF8.GetBytes(content);
+
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: "test.2.queue",
+            basicProperties: null,
+            body);
+
+        Console.WriteLine("sms sent\n\n");
+    }
+
 }
